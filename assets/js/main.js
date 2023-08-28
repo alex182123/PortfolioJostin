51d09c0cea56b356af68d3dc2721b5993292f4e6
Jostin Aguero Castro <jalexisac@hotmail.com>
2023-08-28 00:53:35 -0600

    Finish v0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@
 //     message => alert(message)
 // );
 
+
 function scrollTopWidget() {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -48,20 +49,22 @@ function fadeNavbar() {
     });
 }
 
-$(document).ready(function () {
-    //SCROLLTOP
-    scrollTopWidget();
+function typeWritterEfect(id) {
+    const titleElement = document.getElementById(id);
+    const titleText = titleElement.innerText;
+    titleElement.innerText = ''; // Limpia el contenido original
 
+    let i = 0;
+    const typingEffect = setInterval(() => {
+        titleElement.innerText = titleText.slice(0, i);
+        i++;
+        if (i > titleText.length) {
+            clearInterval(typingEffect);
+        }
+    }, 100); // Ajusta el valor para cambiar la velocidad del efecto
+}
 
-    //YEARCOPYRIGHT
-    getYearCopyright();
-
-    //TOOLTIP
-    tooltip();
-
-    //FADENAVBAR
-    fadeNavbar();
-
+function scrollDownSeeMore() {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('#scrollDownWidget').css('opacity', '0');
@@ -73,4 +76,88 @@ $(document).ready(function () {
     $('#scrollDownWidget').click(function () {
         $('html, body').animate({ scrollTop: $(window).height() }, 600);
     });
+}
+
+function checkSection(idElement) {
+    const element = $('#About');
+    // const element = $(idElement);
+    const windowHeight = $(window).height();
+
+    $(window).scroll(function () {
+        const elementTop = element.offset().top;
+
+        if (elementTop - windowHeight < 0) {
+            // El usuario ha pasado por la sección
+            alert('Usuario ha pasado por la sección ' + idElement);
+        }
+    });
+}
+
+function checkScrolling(idArray) {
+
+    const elements = idArray.map(id => document.getElementById(id));
+    const windowHeight = window.innerHeight;
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    window.addEventListener('scroll', function () {
+        elements.forEach((element, index) => {
+            const elementTop = element.getBoundingClientRect().top;
+            if (elementTop - windowHeight < 0) {
+                links.forEach(link => {
+                    if (link.getAttribute('href') === '#' + idArray[index]) {
+                        link.classList.add('active');
+                        console.log(idArray[index]);
+                    } else {
+                        link.classList.remove('active');
+                    }
+                });
+            }
+        });
+    });
+}
+
+// Ejemplo de uso
+
+// Ejemplo de uso
+
+
+$(document).ready(function () {
+    //SCROLLDOWN
+    scrollDownSeeMore();
+
+    //SCROLLTOP
+    scrollTopWidget();
+
+
+    //YEARCOPYRIGHT
+    getYearCopyright();
+
+    //TOOLTIP
+    tooltip();
+
+    //FADENAVBAR
+    // fadeNavbar();
+
+    //TYPEWRITTEREFFECT
+    typeWritterEfect('home-title');
+
+    //TYPEWRITTEREFFECT
+    typeWritterEfect('about-title');
+
+    //TYPEWRITTEREFFECT
+    typeWritterEfect('projects-title');
+
+    //TYPEWRITTEREFFECT
+    typeWritterEfect('contact-title');
+
+    // Manejar clics en enlaces con desplazamiento suave
+    $('a[href^="#"]').on('click', function (event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 800); // Ajusta la velocidad del desplazamiento si es necesario
+        }
+    });
+    checkScrolling(['Home', 'Projects', 'About', 'Contact']);
 });
